bb331690c38d672ac79a7c6569ce247e110d7613
AJ F <84407852+AJF455@users.noreply.github.com>
2021-05-20 14:47:08 -0400

    Add files via upload

---
+++ Brooks_AJ_JS_Tinkering.js
@@ -0,0 +1,7 @@
+function displayifChildisAbleToRideTheRollerCoaster () {
+} 
+if (childHeight>52)
+console.log ("Get on that ride kiddo!")
+else
+console.log ("Maybe next year.")
+var childHeight=10
